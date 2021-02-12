f681f4a0ba56f8182e6267ee704c58afbae58d4d
Jeju-Boobby <boo1984@naver.com>
2021-02-12 15:49:14 +0900

    사진 수정 및 사진 슬라이드 작업 중

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -41,39 +41,29 @@ $(document).ready(function () {
     })
 })
 
-// Smooth scroll for links with hashes
-$("a.smooth-scroll").click(function (event) {
-    // On-page links
-    if (
-        location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-    ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $("html, body").animate(
-                {
-                    scrollTop: target.offset().top
-                },
-                1000,
-                function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) {
-                        // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    }
-                }
-            );
-        }
-    }
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("card");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active-dot", "");
+  }
+  slides[slideIndex-1].style.display = "table-cell";
+  dots[slideIndex-1].className += " active-dot";
+}
